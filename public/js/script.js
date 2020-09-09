a29e86e66fa42f9089a1a1b68f6a702514f96f12
anu radha <anu.sivasubramanian@gmail.com>
2020-09-09 12:01:36 +0200

    data tables added

---
+++ public/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#keys').DataTable(
+        {"info": false}
+    );
+} );
+
+$(document).ready(function() {
+    $('#domains').DataTable(
+        {"info": false}
+    );
+} );
